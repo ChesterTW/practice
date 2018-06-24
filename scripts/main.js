5eceb320359b21f2d9f4b39220db70fb440aff68
ChesterTW <smalltaro8759387@gmail.com>
2018-06-24 14:39:03 +0800

    'index'

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
-var myhair = document.querySelector('h1');
-myhair.textContent = 'Hello world';
-document.querySelector('html').onclick = function(){
-	alert('良い...良いと快適');
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/dog.jpg') {
+		myImage.setAttribute ('src','images/shydog.jpg');
+	} else {
+		myImage.setAttribute ('src','images/dog.jpg');
+	}
 }
