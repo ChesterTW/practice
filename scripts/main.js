fc4d70c0d9766a2889c7e5a7ec0168377ad08d85
ChesterTW <smalltaro8759387@gmail.com>
2018-06-25 13:59:28 +0800

    'index'

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,20 @@ myImage.onclick = function() {
 		myImage.setAttribute ('src','images/dog.jpg');
 	}
 }
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName(){
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name',myName);
+	myHeading.innerHTML = 'Welcome,' + myName;
+}
+if(!localStorage.getItem('name')) {
+	setUserName();
+}	else {
+	var storedName = localStorage.getItem('name');
+	myHeading.innerHTML = 'Welcome,' + storedName;
+}
+myButton.onclick = function() {
+	setUserName();
+}
